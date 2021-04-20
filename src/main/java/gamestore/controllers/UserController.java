cb1632ed8ff067ae928a2c8f14c588968c8fefa5
dimitar-ivanov-ivanov <dimitar.i.ivanov@abv.bg>
2021-04-20 09:15:30 +0300

    Added more tags to the documentation of the controllers.

--- src/main/java/gamestore/controllers/UserController.java
+++ src/main/java/gamestore/controllers/UserController.java
@@ -16,6 +16,8 @@ import javax.validation.Valid;
 /**
  * The User controller.
  * Only for actions affecting a single user
+ *
+ * @author Dimitar Ivanov
  */
 @AllArgsConstructor
 @RestController
@@ -24,11 +26,15 @@ public class UserController {
 
     /**
      * The service used for internal logic
+     *
+     * @see UserService
      */
     private final UserService userService;
 
     /**
      * The mapper used to parse from user to dto
+     *
+     * @see gamestore.utils.mapper.ModelMapperConfig
      */
     private final ModelMapper mapper;
 
@@ -37,7 +43,11 @@ public class UserController {
      * Only accessible by users who have the user:read authority
      *
      * @param userId the user id
-     * @return the user
+     * @return the found user
+     * @see gamestore.models.entities.security.Authority
+     * @see gamestore.utils.seed.SetupDataLoader -> to find out which initial have the authority
+     * @see UserService#getById(Long)
+     * @see UserGetDto
      */
     @GetMapping(path = "{userId}")
     @PreAuthorize("hasAuthority('user:read')")
@@ -53,6 +63,8 @@ public class UserController {
      * Register new user.
      *
      * @param register the details of the incoming user to be registered
+     * @see UserRegisterBindingModel
+     * @see UserService#registerUser(UserRegisterBindingModel)
      */
     @PostMapping(path = "/register")
     @ResponseStatus(HttpStatus.CREATED)
@@ -65,6 +77,8 @@ public class UserController {
      * Only accessible by users who have the user:delete authority
      *
      * @param userId the user id
+     * @see gamestore.models.entities.security.Authority
+     * @see gamestore.utils.seed.SetupDataLoader -> to find out which initial have the authority
      */
     @DeleteMapping(path = "/delete/{userId}")
     @PreAuthorize("hasAuthority('user:delete')")
@@ -78,11 +92,14 @@ public class UserController {
      *
      * @param userId      the user id
      * @param updatedUser the updated user details
+     * @see gamestore.models.entities.security.Authority
+     * @see gamestore.utils.seed.SetupDataLoader -> to find out which initial have the authority
      */
     @PutMapping(path = "/update/{userId}")
     @PreAuthorize("hasAuthority('user:update')")
     public void updateUser(@PathVariable Long userId,
                            @RequestBody User updatedUser) {
+        //change the request body to binding model -> register binding model?
         throw new NotYetImplementedException();
     }
 }
--- src/main/java/gamestore/controllers/UserManagementController.java
+++ src/main/java/gamestore/controllers/UserManagementController.java
@@ -13,24 +13,35 @@ import java.util.Collection;
 /**
  * The User management controller.
  * Only for actions affecting all users
+ *
+ * @author Dimitar Ivanov
  */
 @AllArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserManagementController {
 
+    /**
+     * The service used for internal logic
+     *
+     * @see UserService
+     */
     private final UserService userService;
 
     /**
      * Gets all users.
      *
      * @return the all users
+     * @see gamestore.models.entities.security.Authority
+     * @see gamestore.utils.seed.SetupDataLoader -> to find out which initial user have the roles
+     * @see UserService#getAllUsers()
      */
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN','ADMIN_TRAINEE')")
     @ResponseBody
     @ResponseStatus(HttpStatus.FOUND)
     public Collection<User> getAllUsers() {
+        //return dtos
         return userService.getAllUsers();
     }
 

c10f084afc346dfecfa36d8a36eaefb514003547
dimitar-ivanov-ivanov <dimitar.i.ivanov@abv.bg>
2021-04-28 08:19:29 +0300

    Added deserialization tests for UserRegisterBindingModel.

--- src/test/java/gamestore/models/bindings/UserRegisterBindingModelTest.java
+++ src/test/java/gamestore/models/bindings/UserRegisterBindingModelTest.java
@@ -1,5 +1,6 @@
 package gamestore.models.bindings;
 
+import gamestore.models.dtos.UserBoughtGameDto;
 import gamestore.models.entities.user.User;
 import gamestore.models.enums.Gender;
 import gamestore.utils.annotations.email.Email;
@@ -8,19 +9,28 @@ import gamestore.utils.constants.TextConstants;
 import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Set;
 
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+@JsonTest
 class UserRegisterBindingModelTest {
+
+    @Autowired
+    private JacksonTester<UserRegisterBindingModel> json;
 
     ValidatorFactory factory;
     Validator validator;
@@ -31,8 +41,21 @@ class UserRegisterBindingModelTest {
     private final String EMAIL_VALID = "nikola@abv.bg";
     private final String PASSWORD_VALID = "A5a381bcacA";
     private final String FIRST_NAME_VALID = "nikola";
-    private final String LAST_NAME_VALID = "siderov";
+    private final String LAST_NAME_VALID = "petrov";
+    private final String BIRTH_DATE_STRING = "1980-05-03";
+    private final String GENDER_VALID = "MALE";
     private final LocalDate BIRTH_DATE_VALID = LocalDate.of(1980, 5, 3);
+
+    private final String JSON_TO_DESERIALIZE =
+            "{\n" +
+                    "        \"username\": \"" + USERNAME_VALID + "\",\n" +
+                    "        \"email\": \"" + EMAIL_VALID + "\",\n" +
+                    "        \"password\": \"" + PASSWORD_VALID + "\",\n" +
+                    "        \"firstName\": \"" + FIRST_NAME_VALID + "\",\n" +
+                    "        \"lastName\": \"" + LAST_NAME_VALID + "\",\n" +
+                    "        \"birthDate\": \"" + BIRTH_DATE_STRING + "\",\n" +
+                    "        \"gender\": \"" + GENDER_VALID + "\"\n" +
+                    "}";
 
     @BeforeEach
     void setUp() {
@@ -153,6 +176,45 @@ class UserRegisterBindingModelTest {
                 .isEqualTo("must not be null");
     }
 
+    @Test
+    public void userNameDeserializes() throws IOException {
+        assertThat(this.json.parseObject(JSON_TO_DESERIALIZE).getUsername())
+                .isEqualTo(USERNAME_VALID);
+    }
 
-    //test deserialization
+    @Test
+    public void emailDeserializes() throws IOException {
+        assertThat(this.json.parseObject(JSON_TO_DESERIALIZE).getEmail())
+                .isEqualTo(EMAIL_VALID);
+    }
+
+    @Test
+    public void passwordDeserializes() throws IOException {
+        assertThat(this.json.parseObject(JSON_TO_DESERIALIZE).getPassword())
+                .isEqualTo(PASSWORD_VALID);
+    }
+
+    @Test
+    public void firstNameDeserializes() throws IOException {
+        assertThat(this.json.parseObject(JSON_TO_DESERIALIZE).getFirstName())
+                .isEqualTo(FIRST_NAME_VALID);
+    }
+
+    @Test
+    public void lastNameDeserializes() throws IOException {
+        assertThat(this.json.parseObject(JSON_TO_DESERIALIZE).getLastName())
+                .isEqualTo(LAST_NAME_VALID);
+    }
+
+    @Test
+    public void birthDateDeserializes() throws IOException {
+        assertThat(this.json.parseObject(JSON_TO_DESERIALIZE).getBirthDate())
+                .isEqualTo(BIRTH_DATE_VALID);
+    }
+
+    @Test
+    public void genderDeserializes() throws IOException {
+        assertThat(this.json.parseObject(JSON_TO_DESERIALIZE).getGender())
+                .isEqualTo(Gender.valueOf(GENDER_VALID));
+    }
 }
